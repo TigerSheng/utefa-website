d623113476a0046c84b0a7e6e7c7816747198ac4
Tiger Sheng <tiger981120@gmail.com>
2018-07-24 19:30:23 -0400

    fixed bug where user log out crashed

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,6 +2,11 @@ import React, {Component, Fragment} from 'react';
 import { Link } from "react-router-dom";
 
 export class NavBar extends Component {
+  constructor(props){
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
   handleLogout(e) {
     this.props.userHasAuthenticated(false);
   }
@@ -11,7 +16,7 @@ export class NavBar extends Component {
       <div className="animated fadeInLeft" id='cssmenu'>
         <ul>
           {this.props.isAuthenticated
-            ? <li><Link to='/' onClick={this.handleLogout}>Log Out</Link></li>
+            ? <li><a onClick={this.handleLogout}>Log Out</a></li>
             : <Fragment>
                 <li><a className="" href='#about'><span>About</span></a></li>
                 <li><a className="" href='#team'><span>Team</span></a></li>
