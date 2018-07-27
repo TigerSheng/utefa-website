7c8cb9e13e39b5ef812a0abb83d25133ff32f78e
Tiger Sheng <tiger981120@gmail.com>
2018-07-27 16:32:38 -0400

    finished delete and get from a specific id
    refactored code using app.param

--- api/app.js
+++ api/app.js
@@ -8,6 +8,34 @@ const uuid = require('uuid/v1');
 
 app.use(bodyParser.json());
 
+//functions to check if certain element exists
+app.param('userId', (req, res, next, id) => {
+  req.userId = id;
+  next();
+})
+app.param('noteId', (req, res, next, id) => {
+  req.noteId = id;
+  const params = {
+    TableName: 'notes',
+    Key: {
+      userId: req.userId,
+      noteId: id
+    }
+  };
+  dynamoDB.get(params, (err, data) => {
+    if(err) return next(err);
+    else if(data.Item){
+      req.notes = data.Item;
+      next();
+    }
+    else{
+      const error = new Error('note does not exist');
+      error.status = 404;
+      return next(error);
+    }
+  });
+});
+
 app.post('/notes', (req, res, next) => {
   console.log(req.body);
 
@@ -22,9 +50,9 @@ app.post('/notes', (req, res, next) => {
   };
   dynamoDB.put(params, (err, data) => {
     if(err)
-      res.send(err);
-    else {
-      res.send("successful");
+      return next(error);
+    else{
+      res.send("post successful");
     }
   });
 });
@@ -32,7 +60,7 @@ app.post('/notes', (req, res, next) => {
 app.get('/notes', (req, res, next) => {
   dynamoDB.scan({TableName: 'notes'}, (err, data) => {
     if(err)
-      res.send(err);
+      return next(err);
     if(data)
       res.send(data.Items);
     else {
@@ -41,8 +69,35 @@ app.get('/notes', (req, res, next) => {
   });
 });
 
+app.get('/notes/:userId/:noteId', (req, res, next) => {
+  res.send(req.notes);
+});
+
 app.get('/', (req, res, next) => {
   res.send('front page');
+});
+
+app.delete('/notes/:userId/:noteId', (req, res, next) => {
+  const params = {
+    Key: {
+      userId: req.userId,
+      noteId: req.noteId
+    },
+    TableName: 'notes'
+  };
+  dynamoDB.delete(params, (err, data) => {
+    if(err){
+      return next(err);
+    }
+    else{
+      res.status(204).send('deleted');
+    }
+  });
+});
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  res.status(status).send(err.message);
 });
 
 module.exports = app;
