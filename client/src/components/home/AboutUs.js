7f246c54291906080306db6a808397f6f9ef7401
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-09-13 10:21:43 -0400

    chhanged grammar

--- client/src/components/home/AboutUs.js
+++ client/src/components/home/AboutUs.js
@@ -13,13 +13,13 @@ export class AboutUs extends Component {
           <div className="emphasis">We are the only engineering finance association.
           </div>
           <div className="about-con">
-            The University of Toronto Engineering Finance Association (UTEFA) is an educational organization that aims to provide students with extensive knowledge about the financial industry by engaging them in investing activities. Since our inception in 2009-2010 UTEFA has grown to more than 55 active and invested members. <br/><br/>
+            The University of Toronto Engineering Finance Association (UTEFA) is an educational organization that aims to provide students with extensive knowledge about the financial industry by engaging them in investing activities. Since our inception in 2009, UTEFA has grown to more than 55 active and invested members. <br/><br/>
             <div className="mission_statement">
               <div className="mission">Our Mission</div>"
                 Provide an opportunity for students to study and understand various sectors of the market through
                 engaging in investing activities."
             </div>
-            We believe the best way to learn, whether the student is an experienced investor or completely new to the stock market, is to take risks and to execute actual investments. As a club we study and invest in companies both from a quantitative and qualitative perspective.
+            We believe the best way to learn, whether the student is an experienced investor or completely new to the stock market, is to take risks and to execute actual investments. As a club, we study and invest in companies from both a qualitative and quantitative perspective.
           </div>
         </div>
       </div>
