487dad756fd79408c74bbce0bc249f797791abd7
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-09-13 00:40:13 -0400

    logo container change
    proper container for the logo so that people can only get to home by actually clicking logo

--- client/src/components/header/Topbar.js
+++ client/src/components/header/Topbar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import './Topbar.css'
 
 export class Topbar extends Component {
   render() {
     return (<div id="top" className="top1">
+    <div className="logo-container">
       <Link to='/'>
         <img id="logo" className="animated fadeInDown" src='./images/UTEFA_LOGO.png'/>
       </Link>
+      </div>
     </div>);
   }
 }
