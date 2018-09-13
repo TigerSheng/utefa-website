acebfe982c092f6bc00b88d105bf6da66bdfcb7c
Tiger Sheng <tiger981120@gmail.com>
2018-09-12 21:18:26 -0400

    Added flow for first time sign-in to change password

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -12,12 +12,23 @@ export default class Login extends Component {
     this.state = {
       email: "",
       password: "",
-      isLoading: false
+      isLoading: false,
+      newPassword: "",
+      confirmNewPassword: "",
+      newUser: null
     };
   }
 
   validateForm() {
     return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  validateChangePasswordForm() {
+    return (
+      this.state.newPassword.length > 0
+      && this.state.confirmNewPassword.length > 0
+      && this.state.newPassword === this.state.confirmNewPassword
+    );
   }
 
   handleChange = event => {
@@ -30,61 +41,131 @@ export default class Login extends Component {
     event.preventDefault();
     this.setState({isLoading: true});
     try {
-      const user = await Auth.signIn(this.state.email, this.state.password)
-        .then(user => console.log(user));
-      this.props.userHasAuthenticated(true);
-      this.props.history.push('/');
+      //log user in, check if first sign in
+      //if first sign in force to change password
+      await Auth.signIn(this.state.email, this.state.password)
+        .then(user => {
+          console.log(user);
+          if(user.challengeName === "NEW_PASSWORD_REQUIRED"){
+            this.setState({
+              newUser: user,
+              isLoading: false
+            });
+          }else{
+            this.props.userHasAuthenticated(true);
+            this.props.history.push('/');
+          }
+        });
     } catch (e) {
       alert(e.message);
       this.setState({isLoading: false});
     }
   }
 
+  handleNewPasswordSubmit = async event => {
+    event.preventDefault();
+    this.setState({ isLoading: true });
+    try{
+      await Auth.completeNewPassword(
+        this.state.newUser,
+        this.state.newPassword,
+        null
+      ).then(() => {
+          Auth.signIn(this.state.email, this.state.newPassword);
+          this.props.userHasAuthenticated(true);
+          this.props.history.push('/');
+      });
+    }catch(e){
+      alert(e.message);
+      this.setState({isLoading: false});
+    }
+  }
+
+  renderLoginPage(){
+    return(
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+            <a href='/forgot-password'>
+              <HelpBlock>Forgot?</HelpBlock>
+            </a>
+          </FormGroup>
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Login"
+            loadingText="Logging in..."
+          >
+            Login
+          </LoaderButton>
+        </form>
+      </div>
+    );
+  }
+
+  renderResetPassword(){
+    return(
+      <div className="Login">
+        <p>Please change your password on your first sign-in.</p>
+        <form onSubmit={this.handleNewPasswordSubmit}>
+          <FormGroup controlId="newPassword" bsSize="large">
+            <ControlLabel>New Password</ControlLabel>
+            <FormControl
+              autoFocus
+              type="password"
+              value={this.state.newPassword}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="confirmNewPassword" bsSize="large">
+            <ControlLabel>Confirm New Password</ControlLabel>
+            <FormControl
+              autoFocus
+              type="password"
+              value={this.state.confirmNewPassword}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <LoaderButton
+            block
+            bsSize="large"
+            disabled={!this.validateChangePasswordForm()}
+            type="submit"
+            isLoading={this.state.isLoading}
+            text="Change Password"
+            loadingText="Logging in..."
+          >
+            Change Password
+          </LoaderButton>
+        </form>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        {this.props.isAuthenticated
-          ? <NavBar
-              isAuthenticated={this.props.isAuthenticated}
-              userHasAuthenticated={this.props.userHasAuthenticated}
-            />
-          : null
-        }
-        <div className="Login">
-          <form onSubmit={this.handleSubmit}>
-            <FormGroup controlId="email" bsSize="large">
-              <ControlLabel>Email</ControlLabel>
-              <FormControl
-                autoFocus
-                type="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </FormGroup>
-            <FormGroup controlId="password" bsSize="large">
-              <ControlLabel>Password</ControlLabel>
-              <FormControl
-                value={this.state.password}
-                onChange={this.handleChange}
-                type="password"
-              />
-              <a href='/forgot-password'>
-                <HelpBlock>Forgot?</HelpBlock>
-              </a>
-            </FormGroup>
-            <LoaderButton
-              block
-              bsSize="large"
-              disabled={!this.validateForm()}
-              type="submit"
-              isLoading={this.state.isLoading}
-              text="Login"
-              loadingText="Logging in..."
-            >
-              Login
-            </LoaderButton>
-          </form>
-        </div>
+        {this.state.newUser === null
+          ? this.renderLoginPage()
+          : this.renderResetPassword()}
       </div>
     );
   }
