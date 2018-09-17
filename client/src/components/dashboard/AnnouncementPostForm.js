8746af8e6d0f0bf5d0b281ed21981581e67e59c3
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-09-17 14:30:40 -0400

    announcementpostform almost complete

---
+++ client/src/components/dashboard/AnnouncementPostForm.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import {
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  Alert,
+  Popover,
+  OverlayTrigger
+} from "react-bootstrap";
+import LoaderButton from '../LoaderButton';
+import "./AnnouncementPostForm.css";
+
+export class AnnouncementPostForm extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+      <form onSubmit={this.props.handleAnnouncementPostFormSubmit}>
+        <FormGroup controlId="title" bsSize="large">
+          <ControlLabel>Title</ControlLabel>
+          <FormControl
+            autoFocus
+            type="Text"
+            onChange={this.props.handleAnnouncementPostChange}
+            value={this.props.title}
+            placeholder="Enter title"
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+        <FormGroup controlId="content" bsSize="large">
+          <ControlLabel>Content</ControlLabel>
+          <FormControl componentClass="textarea"
+          onChange={this.props.handleAnnouncementPostChange}
+           placeholder="Place content here..." 
+           value={this.props.content}/>
+          <FormControl.Feedback />
+        </FormGroup>
+        <FormGroup controlId="attachment" bsSize="large">
+          <ControlLabel>File</ControlLabel>
+          <FormControl
+          type="file"
+          onChange={this.props.handleAnnouncementPostChange}
+          value={this.props.attachment}/>
+          <FormControl.Feedback />
+        </FormGroup>
+        <LoaderButton
+          block
+          bsSize="large"
+          type="submit"
+          isLoading={this.props.isLoading}
+          text="Post"
+          loadingText="Posting..."
+        >
+          Post
+        </LoaderButton>
+      </form>
+      </div>
+    );
+  }
+}
--- client/src/components/dashboard/Post.js
+++ client/src/components/dashboard/Post.js
@@ -2,66 +2,54 @@ import React, { Component } from 'react';
 import './Post.css'
 import {Auth} from 'aws-amplify';
 import {LeftNav} from './LeftNav'
-
-import {
-  HelpBlock,
-  FormGroup,
-  FormControl,
-  ControlLabel,
-  Alert,
-  Popover,
-  OverlayTrigger
-} from "react-bootstrap";
-import LoaderButton from '../LoaderButton';
+import {AnnouncementPostForm} from './AnnouncementPostForm'
 
 export default class  Post extends Component {
   constructor(props) {
     super(props);
+    this.state={
+      title: "",
+      content: "",
+      isLoading: false,
+      attachment: ""
+    }
+  }
+
+  handleAnnouncementPostChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+      });
   }
 
 
+  handleAnnouncementPostFormSubmit = async event => {
+    event.preventDefault();
+    this.setState({isLoading: true});
+    this.setState({
+      title:this.state.title,
+      content:this.state.content,
+      attachment:this.state.attachment
+    });
 
-  render(){
+    console.log(this.state)
+      this.setState({isLoading: false});
+    }
+
+  render() {
+    const announcementPostFormProps = {
+      title: this.state.title,
+      content: this.state.content,
+      isLoading: this.state.isLoading,
+      attachment: this.state.attachment,
+      handleAnnouncementPostFormSubmit:this.handleAnnouncementPostFormSubmit,
+      handleAnnouncementPostChange:this.handleAnnouncementPostChange
+    };
     return(
       <div>
       <LeftNav/>
       <div className="post-view">
       <div className="announcement-post-container">
-      <form onSubmit={this.props.handleSubmit}>
-        <FormGroup controlId="email" bsSize="large"
-          validationState={this.props.isCorrectEmail}>
-          <ControlLabel>Email</ControlLabel>
-          <FormControl
-            autoFocus
-            type="email"
-            value={this.props.email}
-            onChange={this.props.handleChange}
-            placeholder="Enter email"
-          />
-          <FormControl.Feedback />
-        </FormGroup>
-        <FormGroup controlId="password" bsSize="large"
-          validationState={this.props.isCorrectPassword}>
-          <ControlLabel>Password</ControlLabel>
-          <FormControl
-            value={this.props.password}
-            onChange={this.props.handleChange}
-            type="password"
-            placeholder="Enter password"
-          />
-          <FormControl.Feedback />
-        </FormGroup>
-        <LoaderButton
-          block
-          bsSize="large"
-          type="submit"
-          isLoading={this.props.isLoading}
-          text="Login"
-          loadingText="Logging in..."
-        >
-          Login
-        </LoaderButton>
-      </form>
+        <AnnouncementPostForm {...announcementPostFormProps}/>
       </div>
       </div>
       </div>
