1357303018192b4e7f3c6e6318cd7d9b19f9d224
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-09-19 16:10:00 -0400

    changes to vote page

--- client/src/components/dashboard/Vote.js
+++ client/src/components/dashboard/Vote.js
@@ -13,20 +13,35 @@ export default class Vote extends Component {
         stock:"Apple",
         votersYes:["john","mark","ada"],
         votersNo:["nada","joel"],
+        viewer:"mark",
         pitch:null,
+        downloadLink:"https:www.google.com",
+        voteYes: this.voteYes,
+        voteNo: this.voteNo,
         datePosted:Date.now()
       },{
         ticker:"GOOGL",
         stock:"Google",
         votersYes:["john"],
+        viewer:"mark",
         votersNo:["nada","joel"],
         pitch:null,
+        voteYes: this.voteYes,
+        voteNo: this.voteNo,
+        downloadLink:"https:www.google.com",
         datePosted:Date.now()
       }]
     }
   }
 
-
+  voteYes = async event => {
+    //potentially call the updateData function to refresh the data incase of changes (empty->not empty)
+    alert('voted yes!')
+  }
+  voteNo = async event => {
+    //potentially call the updateData function to refresh the data incase of changes (empty->not empty)
+    alert('voted no!')
+  }
   render(){
     return(
       <div>
--- client/src/components/dashboard/VotePost.js
+++ client/src/components/dashboard/VotePost.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import './VotePost.css'
+import {
+  Button,
+  ProgressBar
+} from "react-bootstrap";
 
 export class  VotePost extends Component {
   render(){
     return(
       <div className="voting-container">
       <p className="voting-stock-title">{this.props.votePostData.stock} ({this.props.votePostData.ticker})</p>
-      <div className={this.props.votePostData.votersYes.includes("mark") ? 'hidden' : 'voting-yes-no-vote'}>
-        <button className="vote-yes">Yes</button> <button className="vote-no">No</button>
-      </div>
-      <div className={!this.props.votePostData.votersYes.includes("mark") ? 'hidden' : 'vote-bar'}>
-      <div className="voters-yes" style={{width : 100*(this.props.votePostData.votersYes.length )/(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )+ "%"}}>Yes ({Math.round(100*(this.props.votePostData.votersYes.length /(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )))+ "%"})</div>
-      <div className="voters-no" style={{width : 100*(this.props.votePostData.votersNo.length )/(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )+ "%"}}>No ({Math.round(100*(this.props.votePostData.votersNo.length /(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )))+ "%"})</div>
-      </div>
+      <p className="voting-download-label">If you missed the presentation: <a href={this.props.votePostData.downloadLink}><u className="voting-download-link" >Click to download</u></a></p>
+      {(() => {
+        if(!this.props.votePostData.votersYes.includes(this.props.votePostData.viewer)){
+          return(
+            <div className= 'voting-yes-no-vote'>
+              <Button bsStyle="success" className="vote-yes" onClick={this.props.votePostData.voteYes}>Yes</Button> <Button bsStyle="danger" onClick={this.props.votePostData.voteNo} className="vote-no">No</Button>
+            </div>
+          )
+        }else{
+          return(
+          <div className='vote-bar'>
+          <ProgressBar>
+            <ProgressBar bsStyle="success" now={100*(this.props.votePostData.votersYes.length )/(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )}label={100*(this.props.votePostData.votersYes.length )/(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )+`%`}  />
+            <ProgressBar bsStyle="danger" now={100*(this.props.votePostData.votersNo.length )/(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )} label={100*(this.props.votePostData.votersNo.length )/(this.props.votePostData.votersYes.length+this.props.votePostData.votersNo.length  )+`%`} />
+          </ProgressBar>
+          </div>
+        )
+        }
+      })()
+
+      }
       </div>
 
     );
