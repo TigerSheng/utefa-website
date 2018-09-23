c7ca83a49ab13875d637d2bf3a83daf6df083a70
Tiger Sheng <tiger981120@gmail.com>
2018-09-23 00:21:11 -0400

    added in form prompt for posting votes

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -144,7 +144,7 @@ export default class  Post extends Component {
       this.setState({
         stock: "",
         ticker: "",
-        postvoteSuccess: true
+        postVoteSuccess: true
       })
       return
     })
