f6790dc761d62909c15ff7814d207fb710e0ba8c
Tiger Sheng <tiger981120@gmail.com>
2018-09-25 23:42:36 -0400

    implemented delete learning content

--- client/src/components/learningcontent/LearningContent.js
+++ client/src/components/learningcontent/LearningContent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {LeftNav} from '../LeftNav'
 import './LearningContent.css'
-import {LearningContentItem} from './LearningContentItem'
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import {API, Storage} from 'aws-amplify'
@@ -21,22 +20,15 @@ export default class LearningContent extends Component {
         let files = []
         for(let c of content) {
           let file = c
-          // file.author = c.author
-          // file.contentId = c.contentId
-          // file.description = c.description
-          // file.file = {}
-          // file.file.name = c.file.name
           if(c.file.link){
             Storage.get(c.file.link).then(url =>{
-              file.file.link = url
+              file.file.url = url
               file.postedAt = new Date(c.postedAt).toLocaleString()
               files.push(file)
               this.setState({files})
-            }
-            )
+            })
           }
         }
-
       });
     }catch(e){
       console.log(e)
@@ -48,48 +40,77 @@ export default class LearningContent extends Component {
     return API.get('api', '/learningcontent')
   }
 
+  deleteContent(content) {
+    try{
+      if(content.file.link)
+        Storage.remove(content.file.link)
+      API.del('api', '/learningcontent/'+content.contentId)
+      .then(response => {
+        console.log(response);
+        return window.location.reload();
+      })
+    }catch(e){
+      console.log(e)
+      alert(e)
+    }
+  }
+
   render() {
-    const data=this.state.files
+    const data = this.state.files
     const columns = [{
       Header: 'File',
       accessor: 'file',
       className: "center",
       Cell: props => (
-        <a href={props.value.link}>
+        <a href={props.value.url}>
           <u>{props.value.name}</u>
         </a>
       )
     },
-    {Header: 'Description',
-    accessor: 'description'
-    },{
-        Header: 'Date Posted',
-        accessor: 'postedAt',
-         className: "center"
-      },{
-        Header: 'Author',
-        accessor: 'author',
-         className: "center"
-      },{
-        header: 'Delete',
-        id: 'delete-btn',
-        sortable: false,
-        filterable: false,
-        Cell: props => <div className="delete-content-btn-container"><Button bsStyle="danger">Delete</Button></div>
-        }]
-
-      return (
-            <div>
-              <LeftNav isAdmin={this.props.isAdmin}/>
-               <div className="learning-content-view">
-                <ReactTable
-                  data={data}
-                  columns={columns}
-                  defaultPageSize = {20}
-                />
-                <p className="lc-tip">Tip: Hold shift when sorting to multi-sort</p>
-                </div>
-            </div>
+    {
+      Header: 'Description',
+      accessor: 'description'
+    },
+    {
+      Header: 'Date Posted',
+      accessor: 'postedAt',
+      className: "center"
+    },
+    {
+      Header: 'Author',
+      accessor: 'author',
+      className: "center"
+    },
+    {
+      header: 'Delete',
+      id: 'delete-btn',
+      sortable: false,
+      filterable: false,
+      Cell: props =>  (
+        <div className="delete-content-btn-container">
+          <Button bsStyle="danger"
+            onClick={() => {
+              this.deleteContent(props.row._original)
+            }}
+          >
+            Delete
+          </Button>
+        </div>
       )
+    }]
+
+    return (
+      <div>
+        <LeftNav isAdmin={this.props.isAdmin}/>
+         <div className="learning-content-view">
+          <ReactTable
+            data={data}
+            columns={columns}
+            defaultPageSize = {20}
+          />
+          <p className="lc-tip">Tip: Hold shift when sorting to multi-sort</p>
+          </div>
+      </div>
+    )
   }
 }
