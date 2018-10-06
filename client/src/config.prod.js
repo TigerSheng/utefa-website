76c10054c80a2a52e71dd2df580f8205454f253c
Tiger Sheng <tiger981120@gmail.com>
2018-10-06 15:08:59 -0400

    added config file for prod account

---
+++ client/src/config.prod.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "utefa-website-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: ""
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_7b6sbRmcR",
+    APP_CLIENT_ID: "gukur4pfp5m2cgkvhkupql218",
+    IDENTITY_POOL_ID: "us-east-2:7e3882d3-1df7-4cea-bc22-d3e6fabee912"
+  }
+};
