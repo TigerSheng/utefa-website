7b678ec3ea9cf6130b9b1b948103b2e292cd9c7e
Tiger Sheng <tiger981120@gmail.com>
2018-10-06 19:16:50 -0400

    update dev configs

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Amplify from 'aws-amplify';
-import config from './config.prod.js';
+import config from './config';
 
 Amplify.configure({
   Auth: {
