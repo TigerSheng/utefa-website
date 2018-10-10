6f538d94964b0f13615a0fecec3a2892b39d4e68
Julian Zhang <julian.zhang@mail.utoronto.ca>
2018-10-10 01:06:19 -0400

    Converted LeftNav bar to react-bootstrap Navbar

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,7 +7,11 @@ export default class Dashboard extends Component {
   render(){
     return(
       <div  className="background">
+<<<<<<< HEAD
         <LeftNav userIsAdmin={this.props.userIsAdmin} isAdmin={this.props.isAdmin} userHasAuthenticated={this.props.userHasAuthenticated}/>
+=======
+        <LeftNav isAdmin={this.props.isAdmin} userHasAuthenticated={this.props.userHasAuthenticated}/>
+>>>>>>> Converted LeftNav bar to react-bootstrap Navbar
           <div className="main-view">
             <AnnouncementBlock
             history={this.props.history}
