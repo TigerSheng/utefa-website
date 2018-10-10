58c5cc240a001dcba73ba905a0521bac2eaccf5a
Julian Zhang <julian.zhang@mail.utoronto.ca>
2018-10-10 01:06:19 -0400

    Converted LeftNav bar to react-bootstrap Navbar

--- client/src/components/LeftNav.js
+++ client/src/components/LeftNav.js
@@ -3,6 +3,8 @@ import './LeftNav.css'
 
 import {Auth} from 'aws-amplify';
 import {Link} from 'react-router-dom';
+import { Navbar, Nav, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export class  LeftNav extends Component {
   constructor(props) {
@@ -17,40 +19,84 @@ export class  LeftNav extends Component {
 
   render() {
     return(
-      <div className="LeftNav">
-        <div className="side-logo-container">
-          <Link to='/dashboard'>
-            <img id="leftNavLogo" alt="logo" src='./images/UTEFA_LOGO.png'/>
-          </Link>
-        </div>
-        <div className="LeftNav-Container">
-          <Link to='/dashboard'>
-            <div className={window.location.pathname === "/dashboard" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
-              <p>Home</p>
-            </div>
-          </Link>
-          <Link to='/vote'>
-            <div className={window.location.pathname === "/vote" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
-              <p>Vote</p>
-            </div>
-          </Link>
-          {this.props.isAdmin && <Link to="/post">
-            <div className={window.location.pathname === "/post" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
-              <p>Post</p>
-            </div>
-          </Link>}
-          <Link to="/sessionmaterial">
-            <div className={window.location.pathname === "/sessionmaterial" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
+      <div className="wrapper">
+        <Navbar fluid className="side-bar">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to='/dashboard'>
+                <img id="leftNavLogo" alt="logo" src='./images/UTEFA_LOGO.png'/>
+              </Link>
+            </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <LinkContainer to='/dashboard'>
+            <NavItem className="LeftNav-Item">
+                  <p>Home</p>
+            </NavItem>
+          </LinkContainer>
+
+          <LinkContainer to='/vote'>
+            <NavItem className="LeftNav-Item"> 
+                  <p>Vote</p>
+            </NavItem>
+          </LinkContainer>
+
+          {this.props.isAdmin &&  <LinkContainer to='/post'>
+            <NavItem className="LeftNav-Item"> 
+                  <p>Post</p>
+            </NavItem>
+          </LinkContainer>}
+
+          <LinkContainer to="/sessionmaterial">
+            <NavItem className="LeftNav-Item">
               <p>Materials</p>
-            </div>
-          </Link>
-          <a href="/">
-            <div className="LeftNav-Item" onClick={this.handleLogout}>
-              <p>Log Out</p>
-            </div>
-          </a>
-        </div>
+            </NavItem>
+          </LinkContainer>
+
+          <NavItem className="LeftNav-Item" onClick={this.handleLogout}>
+              <div >
+                <p>Log Out</p>
+              </div>
+          </NavItem>
+        </Nav>
+        </Navbar>
       </div>
+
+
+      // <div className="LeftNav">
+      //   <div className="side-logo-container">
+      //     <Link to='/dashboard'>
+      //       <img id="leftNavLogo" alt="logo" src='./images/UTEFA_LOGO.png'/>
+      //     </Link>
+      //   </div>
+      //   <div className="LeftNav-Container">
+      //     <Link to='/dashboard'>
+      //       <div className={window.location.pathname === "/dashboard" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
+      //         <p>Home</p>
+      //       </div>
+      //     </Link>
+      //     <Link to='/vote'>
+      //       <div className={window.location.pathname === "/vote" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
+      //         <p>Vote</p>
+      //       </div>
+      //     </Link>
+      //     {this.props.isAdmin && <Link to="/post">
+      //       <div className={window.location.pathname === "/post" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
+      //         <p>Post</p>
+      //       </div>
+      //     </Link>}
+      //     <Link to="/sessionmaterial">
+      //       <div className={window.location.pathname === "/sessionmaterial" ? "active-tab LeftNav-Item":"LeftNav-Item"}>
+      //         <p>Materials</p>
+      //       </div>
+      //     </Link>
+      //     <a href="/">
+      //       <div className="LeftNav-Item" onClick={this.handleLogout}>
+      //         <p>Log Out</p>
+      //       </div>
+      //     </a>
+      //   </div>
+      // </div>
     )
   }
 }
