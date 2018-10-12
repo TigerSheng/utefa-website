3b36023ca8a3ee849566b0519e2824172d46d9de
Tiger Sheng <tiger981120@gmail.com>
2018-10-12 19:01:53 -0400

    updated about us

--- client/src/components/home/Decks.js
+++ client/src/components/home/Decks.js
@@ -13,6 +13,7 @@ export default class Decks extends Component {
           <div className="heading">Session Materials</div>
           <div className="decks">
             <div className="emphasis">
+<<<<<<< HEAD
               <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/01_UTEFA-Info-Session.pdf" rel="noopener noreferrer" target="_blank" title="01 - Info Session">01 - Info Session</a>
             </div>
             <div className="emphasis">
@@ -29,6 +30,18 @@ export default class Decks extends Component {
             </div>
             <div className="emphasis">
               <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/Sample-Pitch-DXC-Technology.pdf" rel="noopener noreferrer" target="_blank" title="Sample-Pitch-DXC-Technology">Sample Pitch-DXC Technology</a>
+=======
+              <a className="emphasis" href="decks/01_UTEFA-Info-Session.pdf" ref="noopener noreferrer" target="_blank" title="01 - Info Session">01 - Info Session</a>
+            </div>
+            <div className="emphasis">
+              <a className="emphasis" href="decks/02_UTEFA-Learning-Session-1.pdf" ref="noopener noreferrer" target="_blank" title="02 - Learning Session Part 1">02 - Learning Session Part 1</a>
+            </div>
+            <div className="emphasis">
+              <a className="emphasis" href="decks/02_UTEFA-Learning-Session-1-Accounting-Solutions.pdf" ref="noopener noreferrer" target="_blank" title="02 - Learning Session 1: Accounting-Solutions">02 - Learning Session 1: Accounting-Solutions</a>
+            </div>
+            <div className="emphasis">
+              <a className="emphasis" href="decks/03_UTEFA-Learning-Session-2.pdf" ref="noopener noreferrer" target="_blank" title="03 - Learning Session Part 2">03 - Learning Session Part 2</a>
+>>>>>>> updated about us
             </div>
           </div>
         </div>
