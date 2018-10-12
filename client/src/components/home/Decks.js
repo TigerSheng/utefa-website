f1ab26542eda540dcb47e4367fac7688d4a6fe65
Tiger Sheng <tiger981120@gmail.com>
2018-10-12 19:01:53 -0400

    updated about us

--- client/src/components/home/Decks.js
+++ client/src/components/home/Decks.js
@@ -13,24 +13,6 @@ export default class Decks extends Component {
           <div className="heading">Session Materials</div>
           <div className="decks">
             <div className="emphasis">
-<<<<<<< HEAD
-              <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/01_UTEFA-Info-Session.pdf" rel="noopener noreferrer" target="_blank" title="01 - Info Session">01 - Info Session</a>
-            </div>
-            <div className="emphasis">
-              <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/02_UTEFA-Learning-Session-1.pdf" target="_blank" rel="noopener noreferrer" title="02 - Learning Session Part 1">02 - Learning Session Part 1</a>
-            </div>
-            <div className="emphasis">
-              <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/02_UTEFA-Learning-Session-1-Accounting-Solutions.pdf" rel="noopener noreferrer" target="_blank" title="02 - Learning Session 1: Accounting-Solutions">02 - Learning Session 1: Accounting-Solutions</a>
-            </div>
-            <div className="emphasis">
-              <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/03_UTEFA-Learning-Session-2.pdf" rel="noopener noreferrer" target="_blank" title="03 - Learning Session Part 2">03 - Learning Session Part 2</a>
-            </div>
-            <div className="emphasis">
-              <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/04_UTEFA-Stock-Selection-Guide.pdf" rel="noopener noreferrer" target="_blank" title="04 - UTEFA Stock Selection Guide">04 - UTEFA Stock Selection Guide</a>
-            </div>
-            <div className="emphasis">
-              <a className="emphasis" href="https://s3.us-east-2.amazonaws.com/utefa-client/Sample-Pitch-DXC-Technology.pdf" rel="noopener noreferrer" target="_blank" title="Sample-Pitch-DXC-Technology">Sample Pitch-DXC Technology</a>
-=======
               <a className="emphasis" href="decks/01_UTEFA-Info-Session.pdf" ref="noopener noreferrer" target="_blank" title="01 - Info Session">01 - Info Session</a>
             </div>
             <div className="emphasis">
@@ -41,7 +23,16 @@ export default class Decks extends Component {
             </div>
             <div className="emphasis">
               <a className="emphasis" href="decks/03_UTEFA-Learning-Session-2.pdf" ref="noopener noreferrer" target="_blank" title="03 - Learning Session Part 2">03 - Learning Session Part 2</a>
->>>>>>> updated about us
+              <a className="emphasis" href="decks/01_UTEFA-Info-Session.pdf" ref="noopener noreferrer" target="_blank" title="01 - Info Session">01 - Info Session</a>
+            </div>
+            <div className="emphasis">
+              <a className="emphasis" href="decks/02_UTEFA-Learning-Session-1.pdf" ref="noopener noreferrer" target="_blank" title="02 - Learning Session Part 1">02 - Learning Session Part 1</a>
+            </div>
+            <div className="emphasis">
+              <a className="emphasis" href="decks/02_UTEFA-Learning-Session-1-Accounting-Solutions.pdf" ref="noopener noreferrer" target="_blank" title="02 - Learning Session 1: Accounting-Solutions">02 - Learning Session 1: Accounting-Solutions</a>
+            </div>
+            <div className="emphasis">
+              <a className="emphasis" href="decks/03_UTEFA-Learning-Session-2.pdf" ref="noopener noreferrer" target="_blank" title="03 - Learning Session Part 2">03 - Learning Session Part 2</a>
             </div>
           </div>
         </div>
