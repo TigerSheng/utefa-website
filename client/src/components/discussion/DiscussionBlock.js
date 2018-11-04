5a2d76038b1aa04754ed27b8e5a3e885dac88ddd
Julian Zhang <julian.zhang@mail.utoronto.ca>
2018-11-04 18:14:24 -0500

    Discussion post editing and posting fixed

---
+++ client/src/components/discussion/DiscussionBlock.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import {API} from 'aws-amplify';
+import { DiscussionPost } from './DiscussionPost';
+import { DiscussionPostForm } from './DiscussionPostForm';
+
+import './DiscussionBlock.css';
+
+import {Button} from "react-bootstrap";
+import ReactLoading from 'react-loading';
+import Fade from 'react-reveal/Fade';
+
+import quickSort from "../dashboard/sort";
+
+var index = 5;
+
+export default class DiscussionBlock extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            discussionCreator: false,
+            isLoading: false,
+            posts:[],
+            allPosts:[],
+            moreToLoad:true
+        };
+
+        this.openDiscussionCreator = this.openDiscussionCreator.bind(this);
+        this.closeDiscussionCreator = this.closeDiscussionCreator.bind(this)
+      }
+
+    openDiscussionCreator(){
+      this.setState({discussionCreator: true})
+    }
+
+    closeDiscussionCreator(){
+      this.setState({discussionCreator: false})
+    }
+
+    async componentDidMount() {
+        this.setState({isLoading: true})
+        try{
+          const posts = await this.updateData();
+          quickSort(posts, 0, posts.length-1)
+          posts.reverse();
+          var MTL=true;
+          if (posts.length<=index){
+            MTL=false;
+          }
+          this.setState({
+            posts:posts.slice(0,index),
+            allPosts:posts,
+            moreToLoad:MTL
+          });
+        }catch(e){
+          console.log(e);
+          alert(e.message);
+        }
+        this.setState({isLoading: false})
+      }
+
+    updateData(){
+        const init = {
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            }
+        }
+        return API.get('api', '/discussion', init)
+    }
+
+    loadMore=async event=>{
+        index+=5;
+        var MTL=true;
+        if (this.state.allPosts.length<=index){
+          MTL=false;
+        }
+        this.setState({
+          posts:this.state.allPosts.slice(0,index),
+          moreToLoad:MTL
+        });
+    }
+
+    render(){
+        return(
+          <div>
+            <div>
+              <Button onClick={this.openDiscussionCreator}>Create new discussion</Button>
+              <DiscussionPostForm closeDiscussionCreator={this.closeDiscussionCreator} discussionCreator={this.state.discussionCreator}/>
+            </div>
+            {this.state.isLoading
+              ? <ReactLoading className="loader" type={'spinningBubbles'} color={'white'} />
+              : <Fade big cascade><div>
+                {
+                  this.state.posts.map((index, i) => {
+                    if(this.state.posts[i]) {
+                      return(
+                        <div key={i}>
+                          <DiscussionPost
+                            postData={this.state.posts[i]}
+                            isAdmin={this.props.isAdmin}
+                          />
+                        </div>
+                      )
+                    }else return(null);
+                  })
+                }
+                <div className={this.state.moreToLoad?"button-container":'hidden'}>
+                  <Button onClick= {this.loadMore.bind(this)}>
+                    Load More
+                  </Button>
+                </div>
+                <div className="filler">
+                </div>
+              </div>
+              </Fade>
+            }
+          </div>
+        );
+      }
+
+}
---
+++ client/src/components/discussion/Reply.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import {Auth, API} from 'aws-amplify';
+
+import {Button, Modal, FormControl} from "react-bootstrap";
+
+export class Reply extends Component {
+  constructor(props){
+    super(props);
+  
+    this.state = {
+      deleted: false,
+      editingMode: false,
+      newComment: "",
+      curComment: this.props.replyData.content,
+      userEditableComment: false
+    }
+
+    this.deleteComment = this.deleteComment.bind(this)
+    this.editComment = this.editComment.bind(this)
+    this.openCommentEditor = this.openCommentEditor.bind(this)
+    this.closeCommentEditor = this.closeCommentEditor.bind(this)
+    this.updateNewComment = this.updateNewComment.bind(this)
+  }
+
+  componentDidMount(){
+    Auth.currentAuthenticatedUser().then(user =>{
+      var curUser = this.props.replyData.username === user.username;
+      this.setState({userEditableComment: (curUser | this.props.isAdmin)});
+  });  
+  }
+
+  openCommentEditor(){
+    this.setState({editingMode: true})
+  }
+
+  closeCommentEditor(){
+    this.setState({editingMode: false})
+    this.setState({newComment: ""})
+  }
+
+
+  updateNewComment(e){
+    this.setState({newComment: e.target.value})
+  }
+
+  
+  editComment(){
+    try{
+      const init = {
+        header: {
+          "Content-Type": "application/json"
+        },
+        body: {
+          "content": this.state.newComment
+        },
+        'queryStringParameters': {
+            'replyId': this.props.replyData.replyId,
+            'discussionId': this.props.replyData.discussionId
+          }
+        }
+      Auth.currentAuthenticatedUser().then(user => {
+        API.put('api', '/reply/' + this.props.replyData.replyId, init)
+        .then(response => {
+          this.setState({curComment: this.state.newComment})
+          this.closeCommentEditor()
+        })
+      })
+    }catch(e){
+      console.log(e)
+      alert(e)
+    }
+  }
+
+
+
+
+  deleteComment(){
+    const init = {
+      'queryStringParameters': {
+          'replyId': this.props.replyData.replyId,
+          'discussionId': this.props.replyData.discussionId
+        }
+      }
+    Auth.currentAuthenticatedUser().then(user => {
+        API.del('api', '/reply/' + this.props.replyData.replyId, init)
+        .then(response => {
+          this.setState({deleted:true});
+        })
+    })
+  }
+
+
+  render(){
+
+      return(
+      !this.state.deleted?       
+      <div>
+        <Modal bsSize="small"
+        show={this.state.editingMode}>
+          <Modal.Header className="edit-modal-title">
+            <Modal.Title>Edit Comment</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="edit-modal-text">
+          <FormControl componentClass="textarea" defaultValue={this.props.replyData.content} onChange={this.updateNewComment}/>
+          </Modal.Body>
+          <Modal.Footer className="edit-modal-buttons">
+          <Button onClick={this.editComment} bsStyle="primary">Confirm</Button>
+          <Button onClick={this.closeCommentEditor} bsStyle="danger">Cancel</Button>
+          </Modal.Footer>
+        </Modal>
+
+        {this.state.curComment + "     "}
+        {"author: " + this.props.replyData.author}
+
+        {this.state.userEditableComment && <div>
+        <Button onClick={this.openCommentEditor} bsStyle="primary" bsSize="xsmall">Edit</Button>
+        <Button onClick={this.deleteComment} bsStyle="danger" bsSize="xsmall">x</Button>
+        </div>}
+      </div> 
+      
+      : null
+     );
+
+  }
+}
