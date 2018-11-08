1cac10dd65853934e7b8614acc7855b5dbe87492
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-11-07 22:33:49 -0500

    historical votes implemented

--- client/src/components/vote/Vote.js
+++ client/src/components/vote/Vote.js
@@ -7,6 +7,16 @@ import quickSort from './sort'
 import ReactLoading from 'react-loading'
 import Fade from 'react-reveal/Fade';
 
+//helper function
+const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+// a and b are javascript Date objects
+function dateDiffInDays(a, b) {
+  // Discard the time and time-zone information.
+    const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+    const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  }
 
 export default class Vote extends Component {
   constructor(props) {
@@ -23,8 +33,22 @@ export default class Vote extends Component {
       const votes = await this.getVotes();
       quickSort(votes, 0, votes.length-1);
       votes.reverse();
+      const now = new Date(Date.now())
+      var historicalVotes=[]
+      var recentVotes=[]
+      for(var i in votes){
+        console.log(votes[i])
+        if(dateDiffInDays(new Date(votes[i].time),now)>14){
+          historicalVotes.push(votes[i])
+        }else{
+          recentVotes.push(votes[i])
+        }
+      }
       this.setState({
-        voteData: votes
+        voteData: votes,
+        historicalVotes:historicalVotes,
+        recentVotes:recentVotes,
+        showAll:false
       })
     }catch(e){
       console.log(e)
@@ -36,6 +60,13 @@ export default class Vote extends Component {
   getVotes(){
     return API.get('api', '/votes')
   }
+  toggleHistoricalVotes = async event => {
+    //potentially call the updateData function to refresh the data incase of changes (empty->not empty)
+    this.setState({
+      showAll: !this.state.showAll
+    });
+  }
+
 
   render(){
     return(
@@ -44,12 +75,25 @@ export default class Vote extends Component {
         <Fade big cascade>
         <div className="vote-view">
         {this.state.isLoading && <div className="loadingcontainer"><ReactLoading type={'spinningBubbles'} className="loader" color={'black'} /></div>}
-        {
-          this.state.voteData.length !== 0 && this.state.voteData.map((index, i) => {
-            if(this.state.voteData[i] && this.state.voteData[i].name !== "") {
+        {this.state.recentVotes && this.state.recentVotes.length !== 0 && this.state.recentVotes.map((index, i) => {
+            if(this.state.recentVotes[i] && this.state.recentVotes[i].name !== "") {
               return(
                 <div key={i}>
-                  <VotePost votePostData={this.state.voteData[i]}
+                  <VotePost votePostData={this.state.recentVotes[i]}
+                  isAdmin={this.props.isAdmin}/>
+                </div>
+              )
+            }else return(null);
+          })
+        }
+        <div className="banner" onClick={this.toggleHistoricalVotes.bind(this)}>
+         Historical Votes
+       </div>
+        {this.state.historicalVotes && this.state.historicalVotes.length !== 0 && this.state.historicalVotes.map((index, i) => {
+            if(this.state.historicalVotes[i] && this.state.historicalVotes[i].name !== "") {
+              return(
+                <div className={this.state.showAll? "":"hide"} key={i}>
+                  <VotePost votePostData={this.state.historicalVotes[i]}
                   isAdmin={this.props.isAdmin}/>
                 </div>
               )
