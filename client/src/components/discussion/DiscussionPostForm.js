9fbf3ea0b664d44ec09b0029eaef801af388ae97
amrmahmoud <amrmahmoud.am@hotmail.com>
2018-11-07 21:34:57 -0500

    modal css

--- client/src/components/discussion/DiscussionPostForm.js
+++ client/src/components/discussion/DiscussionPostForm.js
@@ -12,6 +12,7 @@ import {Button, Modal, ModalHeader} from "react-bootstrap";
 import {Auth, API} from 'aws-amplify';
 import LoaderButton from '../LoaderButton';
 import { s3Upload } from "../../libs/awsLib";
+import './DiscussionPostForm.css'
 
 const postPolicy = (
   <Popover id="post-policy">
@@ -99,9 +100,11 @@ export class DiscussionPostForm extends Component {
   }
 
   render() {
-    return(      
+    return(
       <Modal bsSize="large" show={this.props.discussionCreator}>
-      <ModalHeader><Button bsStyle="danger" onClick={this.props.closeDiscussionCreator}>x</Button></ModalHeader>
+      <ModalHeader className="m-header">
+      <button className="close" onClick={this.props.closeDiscussionCreator}>X</button>
+        </ModalHeader>
       <div>
         {this.state.postSuccess
           && <Alert className="Alert" bsStyle='success'>
@@ -113,7 +116,7 @@ export class DiscussionPostForm extends Component {
               Something went wrong. Please try again,
             </Alert>
         }
-        <form onSubmit={this.handleSubmit}>
+        <form className="m-content" onSubmit={this.handleSubmit}>
           <FormGroup controlId="title" bsSize="large">
             <ControlLabel>Title</ControlLabel>
             <FormControl
