8975bc0112a1b0259e1aa9ba20e8440eeaaa5db4
Julian Zhang <19332422+JulianZhang97@users.noreply.github.com>
2019-11-10 18:08:56 -0500

    Added embedded Google Calendar

--- client/src/components/home/Schedule.js
+++ client/src/components/home/Schedule.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Schedule.css'
+import Iframe from 'react-iframe'
 
 export default class Schedule extends Component {
   render() {
@@ -10,7 +11,12 @@ export default class Schedule extends Component {
           <div className="key"><img src="css/key2.png" alt="placeholder"/></div>
           <div className="heading">Schedule and Events</div>
             <div className="schedule">
-              <img src="Schedule.jpg" alt="schedule"/>
+              <Iframe 
+                url="https://calendar.google.com/calendar/b/3/embed?height=600&amp;wkst=1&amp;ctz=America%2FToronto&amp;src=dXRlZmEyMDE5QGdtYWlsLmNvbQ&amp;color=%2322AA99&amp;showCalendars=0&amp;showTz=1&amp;showTabs=1&amp;showPrint=0&amp;showDate=1&amp;showNav=0&amp;showTitle=0&amp;title=UTEFA%20Club%20Calendar"
+                width="1200px"
+                height="720px"
+                display="initial"
+                position="relative"/>
             </div>
         </div>
       </div>
